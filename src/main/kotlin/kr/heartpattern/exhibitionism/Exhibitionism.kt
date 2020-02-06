beff8fc5022086a4d01932738989a8cd6350a3d9
HeartPattern <jun2620354@kaist.ac.kr>
2020-02-06 21:12:18 +0900

    Add progressbar

--- src/main/kotlin/kr/heartpattern/exhibitionism/Exhibitionism.kt
+++ src/main/kotlin/kr/heartpattern/exhibitionism/Exhibitionism.kt
@@ -1,11 +1,11 @@
 package kr.heartpattern.exhibitionism
 
+import me.tongfei.progressbar.ProgressBar
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import java.io.ByteArrayInputStream
 import java.io.FileOutputStream
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
@@ -21,64 +21,48 @@ fun transform(option: ExhibitionismOptions) {
     val input = ZipFile(option.source)
     val output = ZipOutputStream(FileOutputStream(option.destination))
 
-    val resultQueue = LinkedBlockingQueue<Pair<String, ByteArray>>()
-    val transformExecutor = ThreadPoolExecutor(
-        option.parallel,
-        option.parallel,
-        60,
-        TimeUnit.SECONDS,
-        LinkedBlockingQueue()
-    )
-    var totalCount = 0
+    val resultThread = Executors.newSingleThreadExecutor()
+    val transformThread = Executors.newFixedThreadPool(option.parallel)
 
     logger.fine("Start transformation")
+    val progress = ProgressBar("Transform", 0)
 
     for (entry in input.entries()) {
-        if (entry.name.endsWith(".class")) {
-            if (option.path.isEmpty() || option.path.any { entry.name.startsWith(it) }) {
-                logger.fine("Transform class: ${entry.name}")
-                val bytes = input.getInputStream(entry).readBytes()
-                transformExecutor.execute {
-                    val reader = ClassReader(bytes)
-                    val writer = ClassWriter(0)
-                    val transformer = ExhibitionismTransformer(writer, option)
-                    reader.accept(transformer, ClassReader.EXPAND_FRAMES)
-                    resultQueue.put(
-                        entry.name to writer.toByteArray()
-                    )
+        progress.maxHint(progress.max + 1)
+        val bytes = input.getInputStream(entry).readBytes()
+
+        if (entry.name.endsWith(".class")
+            && (option.path.isEmpty() || option.path.any { entry.name.startsWith(it) })) {
+            transformThread.execute {
+                val reader = ClassReader(bytes)
+                val writer = ClassWriter(0)
+                val transformer = ExhibitionismTransformer(writer, option)
+                reader.accept(transformer, ClassReader.EXPAND_FRAMES)
+                resultThread.execute{
+                    output.putNextEntry(ZipEntry(entry.name))
+                    output.write(writer.toByteArray())
+                    output.closeEntry()
+                    progress.step()
                 }
-                totalCount++
-            } else {
-                logger.fine("Copy class: ${entry.name}")
+            }
+        } else {
+            resultThread.execute{
                 output.putNextEntry(ZipEntry(entry.name))
-                ByteArrayInputStream(input.getInputStream(entry).readBytes()).use {
+                ByteArrayInputStream(bytes).use {
                     it.copyTo(output)
                 }
                 output.closeEntry()
+                progress.step()
             }
-        } else {
-            logger.fine("Copy resource: ${entry.name}")
-            output.putNextEntry(ZipEntry(entry.name))
-            ByteArrayInputStream(input.getInputStream(entry).readBytes()).use {
-                it.copyTo(output)
-            }
-            output.closeEntry()
         }
     }
+
+    transformThread.shutdown()
+    resultThread.shutdown()
+    transformThread.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)
+    resultThread.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)
+
     input.close()
-
-    var completeCount = 0
-    while (completeCount < totalCount) {
-        val (name, bytes) = resultQueue.take()
-        output.putNextEntry(ZipEntry(name))
-        output.write(bytes)
-        output.closeEntry()
-        completeCount++
-        if (completeCount % 1000 == 0) {
-            logger.fine("Processing...  (${completeCount}/${totalCount})")
-        }
-    }
-
     output.close()
     logger.fine("Complete transformation")
 }
